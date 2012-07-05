d3df9481551df0d6b21e3c6d9be1ed14dec14e40
robbyzhang <icw_zy@163.com>
2012-07-05 18:10:05 +0800

    1

--- page/index.php
+++ page/index.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-<?php
- echo "hello robby";
-?>
-</body>
+	<head>
+		<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="../css/newchina.css" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="../js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="../js/jquery.form.js"></script>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>project newchina</title>
+		<script   language="javascript">
+			$(document).ready(function() {
+
+			});
+		</script>
+	</head>
+	<body>
+		<div class="container">
+			<table>
+				<tr>
+					<td>
+					<ul class="nav nav-pills" style="clear:left">
+						<li>
+							<a href="">test</a>
+						</li>
+					</ul></td>
+					<td>
+					<div>
+						<form class="form-inline">
+							<input type="text" class="input-small" placeholder="用户名" name="username">
+							<input type="password" class="input-small" placeholder="密码" name="password">
+							<input type="submit"  class="btn" id="login" value="登录">
+							<input type="submit"  class="btn" id="register" value="注册">
+						</form>
+					</div></td>
+				</tr>
+			</tab>
+		</div>
+
+	</body>
 </html>
